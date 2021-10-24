3e58366a584c062ca1d98abe7ffe99b367c98853
Nayeli Pleskac <pleskac510@gmail.com>
2021-10-24 11:22:23 -0700

    showTable function

--- SQLite3-Projects/index.py
+++ SQLite3-Projects/index.py
@@ -15,33 +15,55 @@ def signup(nameOfUser, userNameInput, passwordInput):
 
     c.execute("INSERT INTO signUpUser(name, userName, password) VALUES (?,?,?)",(nameOfUser, userNameInput, passwordInput)) #inserts one row
     conn.commit()
+
     print('SIGN UP SUCCESSFUL')
     print(nameOfUser, userNameInput, passwordInput)
-    c.close()
-    conn.close()
+    
     
 
 def login(userNameInput, passwordInput):
     
-    
     c.execute('SELECT * FROM signUpUser WHERE userName = ? and password = ?', (userNameInput, passwordInput)) #verifies userName / password in signUpUser
+    
     rows = c.fetchall()
+    print(rows)
+
     for r in rows:
         print(r)
         print('welcome ', r[0])
     else:
         print('record not found')
+    
 
 def deleteAccount(userNameInput, passwordInput):
     c.execute('DELETE FROM signUpUser WHERE userName = ? and password = ?', (userNameInput, passwordInput))
+    conn.commit()
+
     rows = c.fetchall()
+    print(rows)
+
     print('record found and deleted')
-    
+
+
 def resetPassword(userNameInput, newPasswordInput):
     c.execute('UPDATE signUpUser SET password = ? WHERE userName = ?', (newPasswordInput, userNameInput))
-    print('passwored reset!')
+    conn.commit()
 
-   
+    rows = c.fetchall()
+    for r in rows:
+        print(r)
+        print('passwored reset!')
+ 
+
+def showTable():
+    c.execute('SELECT * FROM signUpUser')
+    getRows =c.fetchall()
+    for row in getRows: 
+        print(row)
+
+
+
+
 
     # if c.fetchone()[0] == 1:
     #     pass
@@ -58,7 +80,7 @@ print('2. Login')
 print('3. Delete Account')
 print('4. Reset Password')
 
-userInput = input('Enter your choice (1/2/3): ')
+userInput = input('Enter your choice (1/2/3/4): ')
 
 if userInput == '1':
     print('Sign-up page: ')
@@ -70,20 +92,28 @@ if userInput == '1':
         print('passwords do not match!')
     else:
         signup(nameInput, userNameInput, passwordInput)
+        showTable()
+       
 elif userInput == '2':
-    print('Enter your login info:')
     userNameInput = input('enter your username: ')
     passwordInput = input('enter your password: ')
     login(userNameInput, passwordInput)
+    showTable()
+ 
 elif userInput == '3':
     userNameInput = input('enter your username to delete: ')
     passwordInput = input('enter your password to delete: ')
     deleteAccount(userNameInput, passwordInput)
+    showTable()
 elif userInput == '4':
     userNameInput = input('enter your username: ')
     newPasswordInput = input('enter your new password: ')
     resetPassword(userNameInput, newPasswordInput)
+    showTable()
 else:
-    print('enter 1 or 2 to signup/login')
+    print('enter 1/2/3/4')
+
+c.close()
+conn.close()
 
 
