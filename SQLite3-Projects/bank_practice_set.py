877739a15ca7b714b8f49a5253c93ae7bef40b5f
Nayeli Pleskac <pleskac510@gmail.com>
2022-08-28 21:45:56 -0700

    updated db

--- SQLite3-Projects/bank_practice_set.py
+++ SQLite3-Projects/bank_practice_set.py
@@ -1,20 +1,31 @@
 import sqlite3
 
+#self.tblname
+#creating more than one table (users, .., ..)
+#asking the user to select which table to view
+#showTable --> accepts which tblname parameter 
+
 class Database:
     def __init__(self, database_file):
         self.conn= sqlite3.connect(database_file)
         self.c = self.conn.cursor()
         self.time= 0
         self.bankID= None
-    def createTable(self, firstName, lastName, emailID, age, phoneNumber, city):
+        self.dbn = None #database name 
+        self.tn = None #table name
+    def createTable(self):
         self.c.execute("SELECT * FROM sqlite_schema WHERE type='table' AND name= 'users' ")
         if self.c.fetchone() == None: 
             self.c.execute('CREATE TABLE users(userId integer PRIMARY KEY AUTOINCREMENT, firstName text, lastName text, emailId text, age integer, phoneNumber text, city text)') 
-
+        print('table has been created')
+        # self.c.execute("INSERT INTO users(firstName, lastName, emailId, age, phoneNumber, city) VALUES (?,?,?,?,?,?)",(firstName, lastName, emailID, age, phoneNumber, city)) #inserts one row
+        self.conn.commit()
+    def insertRow(self, firstName, lastName, emailID, age, phoneNumber, city):
         self.c.execute("INSERT INTO users(firstName, lastName, emailId, age, phoneNumber, city) VALUES (?,?,?,?,?,?)",(firstName, lastName, emailID, age, phoneNumber, city)) #inserts one row
         self.conn.commit()
     def deleteRow(self, firstName, lastName):
         query = 'SELECT * FROM users'
+        query2 = 'DROP TABLE users'
         self.c.execute(query)
         records= self.c.fetchall()
         for row in records: 
@@ -24,6 +35,7 @@ class Database:
         
         if self.c.fetchone() == None:
             print('user has been deleted')
+        self.conn.commit()
     def updatePhoneNumber(self, phoneNumber, firstName, lastName):
         query = 'SELECT * FROM users'
         self.c.execute(query)
@@ -44,54 +56,85 @@ class Database:
 
 class Bank(Database):
     def __init__(self):
-        Database.__init__(self, 'updated bank practice.db')
+        Database.__init__(self, 'updatedbankpractice.db')
         self.cmd = None
     def interface(self):
-        print('a) create new/insert into database')
-        print('b) edit new database')
+        # print('a) create first row in table')
+        # print('b) edit new table')
+        # print('c) insert into table')
+        # # self.showTable()
+        print('a) select from a table')
+        print('b) select from databases')
+        print('c) create new table')
+        print('d) edit new table')
         self.cmd = input('\n>: ')
     def run(self):
         while True: 
             self.interface()
             if self.cmd == 'a':
+                print('table name: ')
+                self.tn = input('>: ')
+                q = "SELECT * FROM " + self.tn
+                print(q)
+                records= self.c.fetchall()
+                for row in records: 
+                    print(row,'\n')
+                self.c.execute("SELECT * FROM " + self.tn)
+                        
+                # firstNameInput = input('first name: ')
+                # lastNameInput = input('last name: ')
+                # emailInput = input('email: ')
+                # ageInput = input('age: ')
+                # phoneInput = input('phone #: ')
+                # cityInput = input('city: ')
+                # if firstNameInput or lastNameInput or emailInput or phoneInput or cityInput == '':
+                #     print('\n--enter in valid information--\n')
+                # else:
+                # self.createTable(firstNameInput, lastNameInput, emailInput, ageInput, phoneInput, cityInput)
+                # self.showTable()
+            if self.cmd == 'b':
+                print('enter database name: ')
+                self.db = input('>: ')
+                q = self.db + '.db'
+                self.c.execute('.tables')
+                #print out table names in database
+
+
+                # print('c) delete row')
+                # print('d) update phone number')
+                # print('e) close database')
+                # self.cmd = input(':> ')
+                # if self.cmd == 'c':
+                #     firstNameInput = input('first name: ')
+                #     lastNameInput = input('last name: ')
+                #     # if firstNameInput or lastNameInput == '':
+                #     #     print('\n--enter in valid infomation--\n')
+                #     # else:
+                #     self.deleteRow(firstNameInput, lastNameInput)
+                #     self.showTable()
+                # if self.cmd == 'd':
+                #     updatedPhoneInput = input('phone number: ')
+                #     firstNameInput = input('first name: ')
+                #     lastNameInput = input('last name: ')
+                #     # if firstNameInput or lastNameInput == '':
+                #     #     print('\n--enter in valid information--\n')
+                #     # else:    
+                #     self.updatePhoneNumber(updatedPhoneInput, firstNameInput, lastNameInput)
+                #     self.showTable()
+                # if self.cmd == 'e':
+                #     self.close()
+                #     print('closing database')
+                #     break
+            if self.cmd == 'c':
                 firstNameInput = input('first name: ')
                 lastNameInput = input('last name: ')
                 emailInput = input('email: ')
                 ageInput = input('age: ')
                 phoneInput = input('phone #: ')
                 cityInput = input('city: ')
-                if firstNameInput or lastNameInput or emailInput or phoneInput or cityInput == '':
-                    print('\n--enter in valid information--\n')
-                else:
-                    self.createTable(firstNameInput, lastNameInput, emailInput, ageInput, phoneInput, cityInput)
-                    self.showTable()
-            if self.cmd == 'b':
-                print('c) delete row')
-                print('d) update phone number')
-                print('e) close database')
-                self.cmd = input(':> ')
-                if self.cmd == 'c':
-                    firstNameInput = input('first name: ')
-                    lastNameInput = input('last name: ')
-                    if firstNameInput or lastNameInput == '':
-                        print('\n--enter in valid infomation--\n')
-                    else:
-                        self.deleteRow(firstNameInput, lastNameInput)
-                        self.showTable()
-                if self.cmd == 'd':
-                    updatedPhoneInput = input('phone number: ')
-                    firstNameInput = input('first name: ')
-                    lastNameInput = input('last name: ')
-                    if firstNameInput or lastNameInput == '':
-                        print('\n--enter in valid information--\n')
-                    else:    
-                        self.updatePhoneNumber(updatedPhoneInput, firstNameInput, lastNameInput)
-                        self.showTable()
-                if self.cmd == 'e':
-                    self.close()
-                    print('closing database')
-                    break
-            else:
+                self.insertRow(firstNameInput, lastNameInput, emailInput, ageInput, phoneInput, cityInput)
+                self.showTable()
+            elif self.cmd != 'a':
                 print('choose a or b to start')
 
     def showTable(self):
@@ -113,4 +156,5 @@ class App:
 if __name__ == '__main__':
     # database = Database('updated bank practice.db')
     app = App('updatedbankpractice.db')
+    #change file to database name
         
